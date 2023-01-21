ecc8ea20370dee4ef1280d1420baa6aea2877f13
gopal <gopal@student.21-school.ru>
2023-01-21 22:18:33 +0300

    finish exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+const check = (str, bracketsConfig) => {
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  config = bracketsConfig.flat();
+  const stack = [];
+
+  for (const x of str) {
+    let index = config.indexOf(x);
+
+    if (index >= 0) {
+      let last = stack[stack.length - 1];
+      
+      // open brackets
+      if (index % 2 === 0) {
+        // identical brackets
+        if ( x == config[index + 1] && last == x) {
+          stack.pop();
+        } else {
+          stack.push(config[index + 1]);
+        }
+      }
+
+      // close brackets
+      else if (x == last) {
+        stack.pop();
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
+
+module.exports = check;
